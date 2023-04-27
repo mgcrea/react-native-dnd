edf8c626e16bc626c8f4654272733408531d979d
Olivier Louvignes <olivier@mgcrea.io>
2023-04-28 00:44:48 +0200

    feat(update): minor changes

--- docs/src/components/layout/Navigation.tsx
+++ docs/src/components/layout/Navigation.tsx
@@ -38,7 +38,7 @@ export const Navigation: FunctionComponent<NavigationProps> = ({ className, item
                       "block w-full pl-3.5 before:pointer-events-none before:absolute before:-left-1 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full",
                       link.href === currentPage
                         ? "font-semibold text-sky-500 before:bg-sky-500"
-                        : "text-slate-500 before:hidden before:bg-slate-300 hover:text-slate-600 hover:before:block dark:text-slate-400 dark:before:bg-slate-700 dark:hover:text-slate-300"
+                        : "text-slate-500 before:hidden before:bg-slate-300 hover:text-slate-600 hover:before:block dark:text-slate-400 dark:before:bg-secondary-400 dark:hover:text-secondary-200"
                     )}
                   >
                     {link.title}
