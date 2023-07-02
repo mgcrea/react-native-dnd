4b7c1590fe3ee3801a4777c6d1ea48ecc47075f6
Olivier Louvignes <olivier@mgcrea.io>
2023-07-02 23:09:56 +0200

    chore(jest): fix tests

--- test/setup.ts
+++ test/setup.ts
@@ -1,4 +1,4 @@
 import "@testing-library/jest-native/extend-expect";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-require("react-native-reanimated/lib/commonjs/reanimated2/jestUtils").setUpTests();
+require("react-native-reanimated/lib/module/reanimated2/jestUtils").setUpTests();
 jest.mock("react-native-reanimated", () => require("react-native-reanimated/mock"));
