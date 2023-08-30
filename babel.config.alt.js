148319db1a00c29c220f2d117c5a13472ad27040
Olivier Louvignes <olivier@mgcrea.io>
2023-08-30 10:06:57 +0200

    chore(build): provide alt build config

---
+++ babel.config.alt.js
@@ -0,0 +1,14 @@
+/** @type {import('@babel/core').TransformOptions} */
+module.exports = {
+  presets: [["module:metro-react-native-babel-preset", { enableBabelRuntime: false }]],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          src: "./src",
+        },
+      },
+    ],
+  ],
+};
