8e142f8d6d6b915a0819b114d612115ee3dc951f
Olivier Louvignes <olivier@mgcrea.io>
2023-09-12 17:10:48 +0200

    chore(git): remove App.tsx in examples

--- example/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React, {type FunctionComponent} from 'react';
-import {BasicExample} from './BasicExample';
-import {FlatListExample} from './FlatListExample';
-
-export const App: FunctionComponent = () => {
-  // return <BasicExample />;
-  return <FlatListExample />;
-};
